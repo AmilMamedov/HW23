9d00fca079fe56b621b778f1fc8c94170dc4a1b8
AmilMamedov <amil_mamedov_96@mail.ru>
2023-04-07 14:18:01 +0400

    Initial commit

---
+++ HW23/views.py
@@ -0,0 +1,32 @@
+import os
+from flask import Blueprint, Response, request, jsonify
+from werkzeug.exceptions import BadRequest
+
+from work_by_file import WorkByFile
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_DIR = os.path.join(BASE_DIR, "data")
+
+main_bp = Blueprint('main', __name__)
+
+
+@main_bp.route('/perform_query', methods=['POST'])
+def perform_query() -> Response:
+    params = request.json
+    file_path = os.path.join(DATA_DIR, params['file']['filename'])
+
+    if not os.path.exists(file_path):
+        raise BadRequest('file not exist')
+
+    query_builder = WorkByFile(file_path)
+
+    # итерируемся по принятому jsony и применяем заданные фильтры
+    result = None
+    for query in params['queries']:
+        result = query_builder.query(
+            cmd=query['cmd'],
+            value=query['value'],
+            data=result,
+        )
+
+    return jsonify(result)
---
+++ HW23/work_by_file.py
@@ -0,0 +1,38 @@
+from typing import Optional, Iterable
+
+from werkzeug.routing import ValidationError
+
+from functions import filter_query, map_query, unique_query, sort_query, limit_query
+
+# словарик с фильтрами
+CMD_TO_FUNCTION = {
+    'filter': filter_query,
+    'map': map_query,
+    'unique': unique_query,
+    'sort': sort_query,
+    'limit': limit_query,
+}
+
+
+class WorkByFile:
+    def __init__(self, filename):
+        self.filename = filename
+
+    def read_file(self, file_name: str):
+        """Читаем файл с помощью гернератора"""
+        with open(file_name) as file:
+            for row in file:
+                yield row
+
+    def query(self, cmd, value, data: Optional[Iterable[str]]):
+        """применяем заданные фильты к файлу"""
+
+        if cmd not in CMD_TO_FUNCTION.keys():
+            raise ValidationError('cmd contains invalid value')
+
+        if data is None:
+            prepared_data = self.read_file(self.filename)
+        else:
+            prepared_data = data
+        result = CMD_TO_FUNCTION[cmd](param=value, data=prepared_data)
+        return list(result)
